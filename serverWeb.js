79f31443994493e2f9da5cd46a2a766729a7b5ec
Andrea A Martinelli <Andrea@MacBook-Pro-de-Andrea.local>
2016-11-24 10:24:06 -0200

    terceiro commit

--- serverWeb.js
+++ serverWeb.js
@@ -6,8 +6,8 @@ var fs = require("fs"),         // to read static files
     
 var app = http.createServer(handler);
 io = io.listen(app);
-app.listen(3000);
-console.log("http server on port 3000");
+app.listen(8080);
+console.log("http server on port 8080");
 
 
 function handler(req, res){

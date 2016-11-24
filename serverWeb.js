ca1eadc906f3196dfe3b4cdc6a66745f4d703092
Andrea A Martinelli <Andrea@MacBook-Pro-de-Andrea.local>
2016-11-24 10:34:21 -0200

    quarto deploy

--- serverWeb.js
+++ serverWeb.js
@@ -6,8 +6,8 @@ var fs = require("fs"),         // to read static files
     
 var app = http.createServer(handler);
 io = io.listen(app);
-app.listen(8080);
-console.log("http server on port 8080");
+app.listen(3001);
+console.log("http server on port 3001");
 
 
 function handler(req, res){
